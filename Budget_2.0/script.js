5adc19d12d626c9d9e22db62088bb8a22bb8e667
Max Merila <90065604+Max-Merila@users.noreply.github.com>
2022-01-21 23:36:00 -0600

    Add files via upload
    Basic html with starter css. Some javascript from the first budget attempt.

---
+++ Budget_2.0/script.js
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // variables
+// let total = 0;
+// let deposit =[];
+// let withdrawal = [];
+
+// console.log("total is", total)
+
+
+// // function to update total
+// let i = deposit;
+// let j = withdrawal;
+// function updateTotal(){
+//     for(let i=0; i < deposit.length; i++){
+//       total = total+deposit[i];
+//     }
+//     for(let x=0; x<withdrawal.length; x++){
+//         total = total-withdrawal[x];
+//     }
+// };
+
+// // testing
+// deposit.push(5, 8)
+// withdrawal.push(6, 9, 12)
+// console.log("i deposit", deposit )
+// console.log("i withdrawaled", withdrawal)
+
+// updateTotal(); //calling the function updateTotal
+// console.log("updated total is", total )
+// // https://www.youtube.com/watch?v=6BlaKlmeWGk helped me a lot
+// function getDepInput(){
+// var y = document.getElementById("dep_display");
+// var x = document.getElementById("dep").value;
+// y.innerHTML = +x;
+// };
+
+// function getWitInput(){
+//     var y = document.getElementById("wit_display");
+//     var x = document.getElementById("wit").value;
+//     y.innerHTML = +x;
+//     };
+    
+
+// console.log("the total is", total)
